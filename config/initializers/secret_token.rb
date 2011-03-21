482fce6d3e0facd53bc3cde84f7062668ff5e854
Demonchand <demonchand@gmail.com>
2011-03-21 19:19:16 +0530

    my initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeFinance::Application.config.secret_token = '04bc30921d19c601af26451cc750b43561378624b3871bbda7fde70f24a53605eacdccb9ce8948b571cbeb2563366536f5463e138cc3c79e2ba2ff149517c89d'
