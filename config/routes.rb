5508ba6450397b1c65802aa555d188372397cf35
chandru <chandru@xtrafin.net>
2011-03-28 11:26:02 +0530

    home index page is fixed for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 HomeFinance::Application.routes.draw do
-  get "home/index"
-
   resources :expenses
+
+  get "home/index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,5 +59,5 @@ HomeFinance::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
