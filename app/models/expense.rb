338c08f440c44a2ce94de9c7f0785ca19312130a
chandru <chandru@xtrafin.net>
2011-03-30 14:38:01 +0530

    expense model validation is done

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,4 @@
 class Expense < ActiveRecord::Base
- # validates :date, :description, :amount, :presence => true
+  validates :description, :amount, :presence => true
   validates :amount, :numericality => { :greater_then_or_equal_to => 0.01 }
 end
