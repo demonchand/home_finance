7c7657b8605a974a5640175f2d0e50063b360d42
chandru <chandru@xtrafin.net>
2011-03-30 14:39:41 +0530

    fixture file is deleted and some changes mode in factories,expense_test fils

--- test/unit/expense_test.rb
+++ test/unit/expense_test.rb
@@ -2,5 +2,7 @@ require 'test_helper'
 
 class ExpenseTest < Test::Unit::TestCase
   should_not allow_value("dfsfs").for(:amount)
-
+  should validate_presence_of(:description)
+  should_not allow_value("").for(:description)
+  should validate_presence_of(:amount)
 end
