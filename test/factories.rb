bacb9dd51cd2c6a975799300007227264438dafa
chandru <chandru@xtrafin.net>
2011-03-31 17:08:26 +0530

    savings factories is added to the factories directory

--- test/factories.rb
+++ test/factories.rb
@@ -1,9 +1,15 @@
+
 Factory.define :expense do |x|
   x.date "2011-07-03"
   x.description "Its might be"
   x.amount("12.25")
 end
+
 Factory.define :user do |x|
   x.email "abc@gmail.com"
   x.password "123456"
+end
+
+Factory.define :saving do |x|
+  x.balance_amount "12000"
 end
