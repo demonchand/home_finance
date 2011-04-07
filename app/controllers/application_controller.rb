853d9204fdfe3581f9d96c9da5dd053a133850d5
chandru <chandru@xtrafin.net>
2011-04-07 14:59:16 +0530

    go_and_sign_the_register method is writed on application controller and it is used to create the activity values to activity table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  @act = AccountType.all
+  def go_and_sign_the_register(val1, val2, des, amo)
+    s = Activity.create!(:date => Time.now, :description => des, :from => val1, :to => val2, :cash => amo)
+    s.save!
+  end
 end
