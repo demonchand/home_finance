1e11c6a31f546fb2ebdf1e5d5e7570f5a630887f
chandru <chandru@xtrafin.net>
2011-04-07 13:37:46 +0530

    income form is changed to formtastic view and validations are done

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,2 +1,4 @@
 class Income < ActiveRecord::Base
+  validates :name_of_the_income_source, :date, :money, :presence => true
+  validates :money, :numericality => { :greater_then_or_equal_to => 0.1 }
 end
