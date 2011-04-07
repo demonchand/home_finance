478c476e6510bc914d8701e44a8f2e57cd269601
chandru <chandru@xtrafin.net>
2011-04-07 13:45:05 +0530

    income is connnected to the savings

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,4 +1,11 @@
 class Income < ActiveRecord::Base
+
+  after_create :money_goes_to_savings
   validates :name_of_the_income_source, :date, :money, :presence => true
   validates :money, :numericality => { :greater_then_or_equal_to => 0.1 }
+
+  def money_goes_to_savings
+    sav = Saving.find(1)
+    sav.update_attributes(:balance_amount => sav.balance_amount + money)
+  end
 end
