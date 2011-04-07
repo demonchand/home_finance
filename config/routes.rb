129bd850f06a1092226de87db987a40002ef93d9
chandru <chandru@xtrafin.net>
2011-04-07 14:16:43 +0530

    account_type model is created. This is used to store the different types of accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 HomeFinance::Application.routes.draw do
 
+  resources :account_types
+
   resources :incomes
 
   resources :savings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110407073602) do
+ActiveRecord::Schema.define(:version => 20110407084532) do
+
+  create_table "account_types", :force => true do |t|
+    t.string   "account_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "expenses", :force => true do |t|
     t.datetime "date"
