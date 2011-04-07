e9ef051d98977aeac59efcb1d503648aa2f4fadb
chandru <chandru@xtrafin.net>
2011-04-07 12:54:34 +0530

    savings functional controller is done

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,12 @@
 class Expense < ActiveRecord::Base
-  #validates :description, :amount, :presence => true
+
+#  after_create :get_the_amount_from_savings
+
   validates :date, :description, :amount, :presence => true
   validates :amount, :numericality => { :greater_then_or_equal_to => 0.01 }
+
+  def get_the_amount_from_savings
+    s = Saving.find(1)
+    
+  end
 end
