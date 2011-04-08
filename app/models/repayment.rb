de4355923458937f73162b794cafd4dc8d05eaea
chandru <chandru@xtrafin.net>
2011-04-08 14:51:15 +0530

    some validation changes on repayments model

--- app/models/repayment.rb
+++ app/models/repayment.rb
@@ -1,5 +1,5 @@
 class Repayment < ActiveRecord::Base
   belongs_to :loan
-  validates :loan_id, :date, :repayment_amount, :presence => true
+  validates :loan_id, :repayment_date, :repayment_amount, :presence => true
   validates :repayment_amount, :numericality => { :greater_then_or_equal_to => 0.1 }
 end
