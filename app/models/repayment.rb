c83c71352c293c4f8a017b056d5ae6c8dd1f7b2e
chandru <chandru@xtrafin.net>
2011-04-08 15:54:17 +0530

    repaymets is created that change the saving and loan attributes

--- app/models/repayment.rb
+++ app/models/repayment.rb
@@ -1,5 +1,20 @@
 class Repayment < ActiveRecord::Base
+  after_save :repay_the_loan_amount_form_savings
+  
   belongs_to :loan
   validates :loan_id, :repayment_date, :repayment_amount, :presence => true
   validates :repayment_amount, :numericality => { :greater_then_or_equal_to => 0.1 }
+
+  def repay_the_loan_amount_form_savings
+    saving_balance = Saving.find(1)
+    if saving_balance.balance_amount > self.repayment_amount
+      saving_balance.update_attributes(:balance_amount => saving_balance.balance_amount - repayment_amount)
+      loan = Loan.find(saving_balance.loan_id)
+      loan.update_attributes(:status => "paid")
+      saving_balance.save!
+      loan.save!
+    else
+      raise NoMethodError "not enugh to repay the loan amount "
+    end
+  end
 end
