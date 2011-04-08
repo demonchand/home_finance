590e6fa0f96753050185f560217f268c9ae9b4fc
chandru <chandru@xtrafin.net>
2011-04-08 12:24:24 +0530

    validation is done for loan model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,6 @@
 class Loan < ActiveRecord::Base
+
+  validates :giver_name, :loan_start_date, :loan_promise_date, :loan_amount, :presence => true
+  validates :loan_amount, :numericality => { :greater_then_or_equal_to => 0.1 }
+  
 end
