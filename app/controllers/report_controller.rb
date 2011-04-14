a36cc9d6bb6eeebfb54873c3823277f142cf7f9a
chandru <chandru@xtrafin.net>
2011-04-14 10:10:06 +0530

    Now report controller is show the details of total expense and current balance

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,6 +1,6 @@
 class ReportController < ApplicationController
   def index
-    @savings_report = Saving.all
+    @savings_report = Saving.find(1)
     @expense_report = Expense.all
     @loan_report = Loan.all
     @repayment_report = Repayment.all
