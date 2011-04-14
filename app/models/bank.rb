0ff42e38dcd2bb11b4f656ac947314681da531ae
chandru <chandru@xtrafin.net>
2011-04-14 11:16:57 +0530

    bank model validation is done

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,2 +1,4 @@
 class Bank < ActiveRecord::Base
+  validates :account_number, :amount, :transaction_type, :date, :presence => true
+  validates :numericality => { :greater_then_or_equal_to => 1.0 }
 end
