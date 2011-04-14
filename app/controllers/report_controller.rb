ab39f1a53e9b124b418bc4cd8a2705060a52ff01
chandru <chandru@xtrafin.net>
2011-04-14 10:01:16 +0530

    report controller and index files are changed

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,10 @@
 class ReportController < ApplicationController
   def index
+    @savings_report = Saving.all
+    @expense_report = Expense.all
+    @loan_report = Loan.all
+    @repayment_report = Repayment.all
+    @income_report = Income.all
   end
 
 end
